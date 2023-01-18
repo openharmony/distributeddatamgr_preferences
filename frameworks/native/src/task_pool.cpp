b4170b70d5de9b04d22cc5788c1107005e97bb93
wanghuajian <wanghuajian3@huawei.com>
2023-01-18 11:19:12 +0800

    InnerAPI:add Note and tags
    Signed-off-by: wanghuajian <wanghuajian3@huawei.com>

--- frameworks/native/src/task_pool.cpp
+++ frameworks/native/src/task_pool.cpp
@@ -231,13 +231,10 @@ int TaskPool::SpawnThreads(bool isStart)
     int limits = isStart ? minThreads_ : (curThreads_ + 1);
     while (curThreads_ < limits) {
         ++curThreads_;
-
         std::thread thread([this]() {
-            auto realName = std::string("Preferences_TaskWorker");
-            pthread_setname_np(pthread_self(), realName.c_str());
+            pthread_setname_np(pthread_self(), "Preferences_TaskWorker");
             TaskWorker();
         });
-
         LOG_INFO("Spawn task pool threads, min:%d cur:%d max:%d", minThreads_, curThreads_, maxThreads_);
         thread.detach();
     }
