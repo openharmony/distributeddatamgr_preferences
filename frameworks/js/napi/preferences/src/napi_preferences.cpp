8d1878e1c74c302d433a740f8bbb132ec46ad477
renqian <renqian12@huawei.com>
2023-11-30 14:08:16 +0000

    1
    Signed-off-by: renqian <renqian12@huawei.com>

--- frameworks/js/napi/preferences/src/napi_preferences.cpp
+++ frameworks/js/napi/preferences/src/napi_preferences.cpp
@@ -475,8 +475,8 @@ int PreferencesProxy::UnRegisteredAllObservers(RegisterMode mode)
         }
     }
     observers.clear();
-    return hasFailed ? E_ERROR : E_OK;
     LOG_INFO("All observers unsubscribed success.");
+    return hasFailed ? E_ERROR : E_OK;
 }
 } // namespace PreferencesJsKit
 } // namespace OHOS
--- frameworks/js/napi/preferences/src/napi_preferences_helper.cpp
+++ frameworks/js/napi/preferences/src/napi_preferences_helper.cpp
@@ -1,4 +1,4 @@
-/* 
+/*
  * Copyright (c) 2021-2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -60,7 +60,7 @@ int ParseParameters(const napi_env env, napi_value *argv, std::shared_ptr<Helper
             temp = nullptr;
             napi_get_named_property(env, argv[1], DATA_GROUP_ID, &temp);
             napi_valuetype type = napi_undefined;
-            napi_status status = napi_typeof(env, jsValue, &type);
+            napi_status status = napi_typeof(env, temp, &type);
             if (status == napi_ok && (type == napi_null || type == napi_undefined)) {
                 PRE_CHECK_RETURN_ERR_SET(JSUtils::Convert2NativeValue(env, temp, context->dataGroupId) == napi_ok,
                     std::make_shared<ParamTypeError>(DATA_GROUP_ID, "a string."));
