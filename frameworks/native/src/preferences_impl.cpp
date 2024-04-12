1396a1aede6a7057fa37ce7a9443a73740f6f664
李芮 <lirui.wx1229126@huawei.com>
2024-04-12 10:36:13 +0000

    update frameworks/native/src/preferences_impl.cpp. fix
    Signed-off-by: 李芮 <lirui.wx1229126@huawei.com>

--- frameworks/native/src/preferences_impl.cpp
+++ frameworks/native/src/preferences_impl.cpp
@@ -169,7 +169,7 @@ void PreferencesImpl::AwaitLoadFile()
 {
     std::unique_lock<std::mutex> lock(mutex_);
     if (!loaded_) {
-        cond_.wait_for(lock, std::chrono::seconds(WAIT_TIME),[this] { return loaded_; });
+        cond_.wait_for(lock, std::chrono::seconds(WAIT_TIME), [this] { return loaded_; });
     }
 
     if (!loaded_) {
