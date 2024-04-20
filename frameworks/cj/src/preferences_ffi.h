e3e216d452b6857dad712f8a81955454dedb65df
renyi43 <renyi43@huawei.com>
2024-04-20 17:09:02 +0800

    add preferences api
    Signed-off-by: renyi43 <renyi43@huawei.com>

---
+++ frameworks/cj/src/preferences_ffi.h
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef PREFERENCES_FFI_H
+#define PREFERENCES_FFI_H
+
+#include "napi_base_context.h"
+#include "ffi_remote_data.h"
+#include "cj_common_ffi.h"
+#include "preferences_log.h"
+#include "preferences_impl.h"
+#include "preferences_interface.h"
+
+#include <cstdint>
+
+namespace OHOS {
+namespace Preferences {
+    
+extern "C" {
+    FFI_EXPORT int64_t FfiOHOSPreferencesGetPreferences(OHOS::AbilityRuntime::Context* context, const char* name,
+        const char* dataGroupId, int32_t* errCode);
+
+    FFI_EXPORT int32_t FfiOHOSPreferencesDeletePreferences(OHOS::AbilityRuntime::Context* context, const char* name,
+        const char* dataGroupId);
+
+    FFI_EXPORT int32_t FfiOHOSPreferencesRemovePreferencesFromCache(OHOS::AbilityRuntime::Context* context,
+        const char* name, const char* dataGroupId);
+
+    FFI_EXPORT int32_t FfiOHOSPreferencesDelete(int64_t id, const char* key);
+
+    FFI_EXPORT bool FfiOHOSPreferencesHas(int64_t id, const char* key);
+
+    FFI_EXPORT int32_t FfiOHOSPreferencesOn(int64_t id, const char* mode, void (*callback)(const char* value),
+        void (*callbackRef)(const char* valueRef));
+
+    FFI_EXPORT int32_t FfiOHOSPreferencesOff(int64_t id, const char* mode, void (*callback)(const char* value));
+
+    FFI_EXPORT int32_t FfiOHOSPreferencesOffAll(int64_t id, const char* mode);
+
+    FFI_EXPORT ValueType FfiOHOSPreferencesGet(int64_t id, const char* key, ValueType defValue);
+
+    FFI_EXPORT int32_t FfiOHOSPreferencesPut(int64_t id, const char* key, ValueType value);
+
+    FFI_EXPORT ValueTypes FfiOHOSPreferencesGetAll(int64_t id);
+
+    FFI_EXPORT void FfiOHOSPreferencesFlush(int64_t id);
+
+    FFI_EXPORT void FfiOHOSPreferencesClear(int64_t id);
+}
+
+} // namespace Preferences
+} // namespace OHOS
+
+#endif
---
+++ frameworks/cj/src/preferences_mock.cpp
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "cj_common_ffi.h"
+
+extern "C" {
+FFI_EXPORT int FfiOHOSPreferencesGetPreferences = 0;
+FFI_EXPORT int FfiOHOSPreferencesDeletePreferences = 0;
+FFI_EXPORT int FfiOHOSPreferencesRemovePreferencesFromCache = 0;
+FFI_EXPORT int FfiOHOSPreferencesDelete = 0;
+FFI_EXPORT int FfiOHOSPreferencesHas = 0;
+FFI_EXPORT int FfiOHOSPreferencesOn = 0;
+FFI_EXPORT int FfiOHOSPreferencesOff = 0;
+FFI_EXPORT int FfiOHOSPreferencesOffAll = 0;
+FFI_EXPORT int FfiOHOSPreferencesGet = 0;
+FFI_EXPORT int FfiOHOSPreferencesPut = 0;
+FFI_EXPORT int FfiOHOSPreferencesGetAll = 0;
+FFI_EXPORT int FfiOHOSPreferencesFlush = 0;
+FFI_EXPORT int FfiOHOSPreferencesClear = 0;
+}
