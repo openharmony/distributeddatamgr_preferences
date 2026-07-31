f11f54699b56bfc31e6823cab061bb5ae06dcdb3
wangjiaqiang <wangjiaqiang11@h-partners.com>
2026-07-31 09:34:35 +0800

    重构xml架构
    Signed-off-by: wangjiaqiang <wangjiaqiang11@h-partners.com>
    
    AI[100%] Human Fixed[0%] Human[0%] AI Adopted[0%]

--- frameworks/native/src/base64_helper.cpp
+++ frameworks/native/src/base64_helper.cpp
@@ -12,6 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 #include "base64_helper.h"
 #include <vector>
 #include "log_print.h"
